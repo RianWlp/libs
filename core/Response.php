ccb3f9a48830769673eecf31be0c508fd73dae23
RianWlp <rianwlp@gmail.com>
2024-11-21 23:38:30 -0300

    fix: cors

--- core/Response.php
+++ core/Response.php
@@ -28,19 +28,52 @@ class Response
 
     public static function sendJsonResponse($data, int $statusCode = 200, string $message = ''): void
     {
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-        header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-        header('Access-Control-Allow-Credentials: true');
-        header('Content-Type: application/json');
-        http_response_code($statusCode);  // Defina o código do status HTTP
+        // Defina os cabeçalhos apenas uma vez
+        if (!headers_sent()) {
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+            header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+            header('Access-Control-Allow-Credentials: true');
+            header('Content-Type: application/json');
+        }
 
+        // Tratamento para requisições OPTIONS (preflight)
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            http_response_code(204);
+            exit;
+        }
+
+        // Define o código do status HTTP
+        http_response_code($statusCode);
+
+        // Monta a resposta JSON
         $response = [
             'status'  => $statusCode,
             'message' => $message,
-            'data'    => $data
+            'data'    => $data,
         ];
+
+        // Envia a resposta
         echo json_encode($response);
-        exit();  // Finaliza a execução para garantir que a resposta seja enviada corretamente
+        exit;
     }
+
+
+    // public static function sendJsonResponse($data, int $statusCode = 200, string $message = ''): void
+    // {
+    //     header('Access-Control-Allow-Origin: *');
+    //     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    //     header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+    //     header('Access-Control-Allow-Credentials: true');
+    //     header('Content-Type: application/json');
+    //     http_response_code($statusCode);  // Defina o código do status HTTP
+
+    //     $response = [
+    //         'status'  => $statusCode,
+    //         'message' => $message,
+    //         'data'    => $data
+    //     ];
+    //     echo json_encode($response);
+    //     exit();  // Finaliza a execução para garantir que a resposta seja enviada corretamente
+    // }
 }
