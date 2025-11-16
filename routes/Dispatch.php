809a6aa1158b0bcb2026d1e2eac58aa17ffa94b1
RianWlp <rianwlp@gmail.com>
2025-11-16 02:21:09 -0300

    change: melhorias no Dispatch

--- routes/Dispatch.php
+++ routes/Dispatch.php
@@ -9,7 +9,7 @@ class Dispatch
 {
     public const DEFAULT_NAMESPACE = 'App\\controllers\\api\\';
 
-    public function dispach($callback, $params = [], $namespace = self::DEFAULT_NAMESPACE, $isAuthenticated = true)
+    public function dispach($callback, $params = [], $namespace = self::DEFAULT_NAMESPACE, $is_authenticated = true)
     {
         if (is_callable($callback['callback'])) {
             return call_user_func_array($callback['callback'], array_values($params));
@@ -29,34 +29,34 @@ class Dispatch
         }
 
         // Inicializando as variáveis para log
-        $endpoint     = $callback['uri'] ?? 'desconhecido';
-        $httpMethod   = $_SERVER['REQUEST_METHOD'] ?? 'http method desconhecido';
-        $dataRequest  = array_merge($_REQUEST, $params) ?? [];
-        $dataResponse = null;
-        $statusCode   = 200;
-        $ipOrigem     = $_SERVER['REMOTE_ADDR'] ?? 'ip desconhecido';
+        $endpoint      = $callback['uri'] ?? 'desconhecido';
+        $http_method   = $_SERVER['REQUEST_METHOD'] ?? 'http method desconhecido';
+        $data_request  = array_merge($_REQUEST, $params) ?? [];
+        $data_response = null;
+        $status        = 200;
+        $ip_origem     = $_SERVER['REMOTE_ADDR'] ?? 'ip desconhecido';
 
         try {
             // Chamar o método no controller
-            $dataResponse = call_user_func_array([new $controller, $method], [(object)$dataRequest]);
+            $data_response = call_user_func_array([new $controller, $method], [(object)$data_request]);
         } catch (\Exception $e) {
-            $dataResponse = ['error' => $e->getMessage()];
-            $statusCode   = 500; // Código de erro interno
+            $data_response = ['error' => $e->getMessage()];
         }
-        // $this->storeLog($endpoint, $httpMethod, $dataRequest, $dataResponse, (string)$statusCode, $ipOrigem);
-        return $dataResponse;
+
+        // $this->storeLog($endpoint, $httpMethod, $dataRequest, $data_response, (string)$statusCode, $ipOrigem);
+        echo $data_response;
     }
 
-    private function storeLog(string $endpoint, string $method, ?array $dataRequest, ?array $dataResponse, string $statusCode, ?string $ipOrigem): void
+    private function storeLog(string $endpoint, string $method, ?array $data_request, ?array $data_response, string $status, ?string $ip_origem): void
     {
         $log = new Log(new DbConnect());
 
         $log->setEndpoint($endpoint);
         $log->setMetodo($method);
-        $log->setDadosRequisicao($dataRequest);
-        $log->setDadosResposta($dataResponse);
-        $log->setStatusHttp($statusCode);
-        $log->setIpOrigem($ipOrigem);
+        $log->setDadosRequisicao($data_request);
+        $log->setDadosResposta($data_response);
+        $log->setStatusHttp($status);
+        $log->setIpOrigem($ip_origem);
         $log->store();
     }
 }
